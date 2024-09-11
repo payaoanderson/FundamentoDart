26c206969c001f14372f41f278ab6825f14e247b
payaoanderson <andersonpayao018@gmail.com>
2024-09-10 22:17:03 -0300

    listas

---
+++ fundamentos/lib/listas/1_declaracao.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  List<int> listaNumeros = [1, 2, 3];
+
+  List<String> listaTextos = ['fulano', 'beltrano', 'sicrano'];
+
+  var listaNumerosInferencias = [1, 2, 3];
+
+  var listaTextosInferencias = ['fulano', 'beltrano', 'sicrano'];
+
+  List<int> listaSemDadosGenericos = [];
+
+  var listaSemDadosInferencias = [];
+
+  var listaSemDadosInferenciaGenerico = <int>[];
+
+  var listaSemDadosInferenciaGenericoString = <String>[];
+}
