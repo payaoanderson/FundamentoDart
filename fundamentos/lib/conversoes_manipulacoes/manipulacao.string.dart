66c35051e0d42060713064957391632ebb629ab4
payaoanderson <andersonpayao018@gmail.com>
2024-09-11 22:17:39 -0300

    manipulaçao de string

---
+++ fundamentos/lib/conversoes_manipulacoes/manipulacao.string.dart
@@ -0,0 +1,9 @@
+void main() {
+  final nome = "fulano de tal";
+
+  var nome_substring = nome.substring(7);
+  print(nome_substring);
+
+  nome_substring = nome.substring(0,6);
+  print(nome_substring);
+}
